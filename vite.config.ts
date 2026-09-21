f6422aec37c55e78f776f9c9f88936ac3ed971c1
souravkrtarafdar <souravkrtarafdar@gmail.com>
2026-09-21 22:25:57 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
-// https://vitejs.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [react()],
   resolve: {
